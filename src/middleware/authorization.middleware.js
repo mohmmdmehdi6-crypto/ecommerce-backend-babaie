ace4e55d3104f729211781e34461cb94088d76ab
mohmmdmehdi6-crypto <mohmmdmehdi6@gmail.com>
2026-09-20 17:07:06 +0330

    feat:(authorization): add user and admin authorization

--- src/middleware/authorization.middleware.js
+++ src/middleware/authorization.middleware.js
@@ -1,11 +1,19 @@
-import { customError } from "./error.middleware";
+import { customError } from "./error.middleware.js";
 
 export const checkAuthorization = (request, response, next, role) => {
   const user = request.user;
+
   if (!user) {
     customError("User token is not valid", 401);
   }
+
   if (user.role !== role) {
     customError("You don't have access for this action", 403);
   }
+
+  next();
+};
+
+export const checkAuthorizationAdmin = (request, response, next) => {
+  checkAuthorization(request, response, next, "ADMIN");
 };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -7,13 +7,31 @@ import {
 } from "../controllers/product.controller.js";
 import { deleteProduct } from "../controllers/product.controller.js";
 import { upload } from "../utils/multer.utils.js";
+import { checkAuthorizationAdmin } from "../middleware/authorization.middleware.js";
+import { checkAuthentication } from "../middleware/auth.middleware.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/", getProducts);
 productRouter.get("/:id", getProductById);
-productRouter.post("/", upload.single("image"), createProduct);
-productRouter.put("/:id", updateProduct);
-productRouter.delete("/:id", deleteProduct);
+productRouter.post(
+  "/",
+  checkAuthentication,
+  checkAuthorizationAdmin,
+  upload.single("image"),
+  createProduct,
+);
+productRouter.put(
+  "/:id",
+  checkAuthentication,
+  checkAuthorizationAdmin,
+  updateProduct,
+);
+productRouter.delete(
+  "/:id",
+  checkAuthentication,
+  checkAuthorizationAdmin,
+  deleteProduct,
+);
 
 export { productRouter };
