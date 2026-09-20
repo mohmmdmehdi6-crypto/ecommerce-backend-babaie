7bb81457249fc1a275a0da646a43320f25b1d247
mohmmdmehdi6-crypto <mohmmdmehdi6@gmail.com>
2026-09-20 16:40:42 +0330

    feat:(auth): add authorization middleware

---
+++ src/middleware/authorization.middleware.js
@@ -0,0 +1,11 @@
+import { customError } from "./error.middleware";
+
+export const checkAuthorization = (request, response, next, role) => {
+  const user = request.user;
+  if (!user) {
+    customError("User token is not valid", 401);
+  }
+  if (user.role !== role) {
+    customError("You don't have access for this action", 403);
+  }
+};
